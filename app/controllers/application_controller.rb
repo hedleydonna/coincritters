afda5590c9344d25480b00113d001d66a55f51ef
Donna Hedley <hedleydonna@gmail.com>
2025-12-01 17:42:24 -0500

    Clean up: back to minimal ApplicationController like Streakling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
   
-    # This single line is the magic that makes login go to dashboard instead of root
-    def after_sign_in_path_for(resource)
-      dashboard_path
-    end
+    # # This single line is the magic that makes login go to dashboard instead of root
+    # def after_sign_in_path_for(resource)
+    #   dashboard_path
+    # end
   end
-  
+
