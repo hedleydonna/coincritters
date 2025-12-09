2040733e22be805360db88d0b816c8c5948b800c
Donna Hedley <hedleydonna@gmail.com>
2025-12-08 20:52:19 -0500

    Fix Render deployment - create simple bin/bundle binstub and update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -22,4 +22,4 @@ gem cleanup stringio
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rails db:migrate
+RAILS_ENV=production bundle exec rails db:migrate
