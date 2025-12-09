fcb7334407c2198dcd7787e2673caedb30f42560
Donna Hedley <hedleydonna@gmail.com>
2025-12-09 12:24:44 -0500

    fix table error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  # Ensure display_name attribute is accessible
-  def display_name
-    self[:display_name]
-  end
-
-  def display_name=(value)
-    self[:display_name] = value
-  end
 end
