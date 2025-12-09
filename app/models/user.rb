e3a2f33d8eb8b3422837ed5f9b403d1b82b4cd33
Donna Hedley <hedleydonna@gmail.com>
2025-12-09 12:20:32 -0500

    Force Rails to reload column information for display_name attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Reset column information to ensure display_name is recognized
+  self.reset_column_information
 end
